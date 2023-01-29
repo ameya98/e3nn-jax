29c0ce5846ea9f6bb55dbb6536cd515b858c21e3
Mario Geiger <geiger.mario@gmail.com>
2023-01-28 22:29:12 -0500

    move s2grid_vectors as SphericalSignal.grid_vector

--- e3nn_jax/__init__.py
+++ e3nn_jax/__init__.py
@@ -73,11 +73,7 @@ from e3nn_jax._src.reduced_tensor_product import (
     reduced_symmetric_tensor_product_basis,
     reduced_antisymmetric_tensor_product_basis,
 )
-<<<<<<< HEAD
 from e3nn_jax._src.s2grid import s2_irreps, to_s2grid, to_s2point, from_s2grid, s2_sum_of_diracs, SphericalSignal
-=======
-from e3nn_jax._src.s2grid import from_s2grid, to_s2grid, s2_irreps, SphericalSignal
->>>>>>> 765b345 (add TODOs)
 
 # make submodules flax and haiku available
 from e3nn_jax import flax, haiku
@@ -169,13 +165,10 @@ __all__ = [
     "reduced_symmetric_tensor_product_basis",
     "reduced_antisymmetric_tensor_product_basis",
     "s2_irreps",
-<<<<<<< HEAD
     "to_s2grid",
     "to_s2point",
     "from_s2grid",
     "s2_sum_of_diracs",
-=======
->>>>>>> 765b345 (add TODOs)
     "SphericalSignal",
     "flax",
     "haiku",
