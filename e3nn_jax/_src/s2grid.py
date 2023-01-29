ad38cb82a71e849cb89b67f500a4d3300a2803fe
Ameya Daigavane <ameya.d.98@gmail.com>
2023-01-28 21:17:30 -0500

    to_s2grid() and from_s2grid() now work with SphericalSignals.

--- e3nn_jax/_src/s2grid.py
+++ e3nn_jax/_src/s2grid.py
@@ -757,6 +757,87 @@ def to_s2point(
     coeffs: e3nn.IrrepsArray,
     point: e3nn.IrrepsArray,
     *,
+    normalization: str = "integral",
+) -> e3nn.IrrepsArray:
+    """Evaluate a signal on the sphere given by the coefficient in the spherical harmonics basis.
+
+    It computes the same thing as :func:`e3nn_jax.to_s2grid` but at a single point.
+
+    Args:
+        coeffs (`e3nn_jax.IrrepsArray`): coefficient array of shape ``(*shape1, irreps)``
+        point (`jax.numpy.ndarray`): point on the sphere of shape ``(*shape2, 3)``
+        normalization ({'norm', 'component', 'integral'}): normalization of the basis
+
+    Returns:
+        `jax.numpy.ndarray`: the value of the signal at the point, of shape ``(*shape1, *shape2, irreps)``
+    """
+    coeffs = coeffs.regroup()
+
+    if not all(mul == 1 for mul, _ in coeffs.irreps):
+        raise ValueError(f"Multiplicities should be ones. Got {coeffs.irreps}.")
+
+    if not isinstance(point, e3nn.IrrepsArray):
+        raise TypeError(f"point should be an e3nn.IrrepsArray, got {type(point)}.")
+
+    if point.irreps not in ["1e", "1o"]:
+        raise ValueError(f"point should be of irreps '1e' or '1o', got {point.irreps}.")
+
+    p_arg = point.irreps[0].ir.p
+    p_val, _ = _check_parities(coeffs.irreps, None, p_arg)
+
+    sh = e3nn.spherical_harmonics(coeffs.irreps.ls, point, True, "integral")  # [*shape2, irreps]
+    n = _normalization(sh.irreps.lmax, normalization, coeffs.dtype, "to_s2")[jnp.array(sh.irreps.ls)]  # [num_irreps]
+    sh = sh * n
+
+    shape1 = coeffs.shape[:-1]
+    coeffs = coeffs.reshape((-1, coeffs.shape[-1]))
+    shape2 = point.shape[:-1]
+    sh = sh.reshape((-1, sh.shape[-1]))
+
+    return e3nn.IrrepsArray(
+        {1: "0e", -1: "0o"}[p_val], jnp.einsum("ai,bi->ab", sh.array, coeffs.array).reshape(shape1 + shape2 + (1,))
+    )
+
+    def _compute_parities(p_even: Optional[int], p_odd: Optional[int]) -> Tuple[Optional[int], Optional[int]]:
+        """Maps (p_even, p_odd) -> (p_val, p_arg)."""
+        computed_p_val = p_even
+        try:
+            computed_p_arg = p_even * p_odd
+        except TypeError:
+            computed_p_arg = None
+        return computed_p_val, computed_p_arg
+
+    def _check_consistency(provided_val: Optional[int], computed_val: Optional[int], name: str) -> int:
+        """Checks that the provided value and the computed value are consistent."""
+        # Exactly one of the values is not None?
+        # Then, we return the non-None value.
+        no_check = (provided_val is None) ^ (computed_val is None)
+        if no_check:
+            return provided_val or computed_p_val
+
+        # Both are None?
+        # Then, we must error out.
+        if provided_val is None:
+            raise ValueError(f"Could not compute a value for {name}. Please provide a value.")
+
+        # Both are not None.
+        # Then, both of the values should be equal.
+        if provided_val != computed_val:
+            raise ValueError(f"Provided parity {name} {p_val} inconsistent with {p_val} computed from coeffs.")
+        return provided_val
+
+    p_even = _extract_element({ir.p for _, ir in coeffs.irreps if ir.l % 2 == 0})
+    p_odd = _extract_element({ir.p for _, ir in coeffs.irreps if ir.l % 2 == 1})
+    computed_p_val, computed_p_arg = _compute_parities(p_even, p_odd)
+    p_val = _check_consistency(p_val, computed_p_val, "p_val")
+    p_arg = _check_consistency(p_arg, computed_p_arg, "p_arg")
+    return SphericalSignal(signal, quadrature=quadrature, p_val=p_val, p_arg=p_arg)
+
+
+def to_s2point(
+    coeffs: e3nn.IrrepsArray,
+    point: e3nn.IrrepsArray,
+    *,
     normalization: str = "component",
 ) -> e3nn.IrrepsArray:
     """Evaluate a signal on the sphere given by the coefficient in the spherical harmonics basis.
@@ -797,48 +878,6 @@ def to_s2point(
     return e3nn.IrrepsArray(
         {1: "0e", -1: "0o"}[p_val], jnp.einsum("ai,bi->ab", sh.array, coeffs.array).reshape(shape1 + shape2 + (1,))
     )
-    # Compute the parity of the irreps, and check that they are consistent with user input.
-    def _extract_element(seq: Sequence[int]) -> int:
-        """Extracts the first element of the sequence, otherwise None."""
-        for e in seq:
-            return e
-        return None
-
-    def _compute_parities(p_even: Optional[int], p_odd: Optional[int]) -> Tuple[Optional[int], Optional[int]]:
-        """Maps (p_even, p_odd) -> (p_val, p_arg)."""
-        computed_p_val = p_even
-        try:
-            computed_p_arg = p_even * p_odd
-        except TypeError:
-            computed_p_arg = None
-        return computed_p_val, computed_p_arg
-
-    def _check_consistency(provided_val: Optional[int], computed_val: Optional[int], name: str) -> int:
-        """Checks that the provided value and the computed value are consistent."""
-        # Exactly one of the values is not None?
-        # Then, we return the non-None value.
-        no_check = (provided_val is None) ^ (computed_val is None)
-        if no_check:
-            return provided_val or computed_p_val
-
-        # Both are None?
-        # Then, we must error out.
-        if provided_val is None:
-            raise ValueError(f"Could not compute a value for {name}. Please provide a value.")
-
-        # Both are not None.
-        # Then, both of the values should be equal.
-        if provided_val != computed_val:
-            raise ValueError(f"Provided parity {name} {p_val} inconsistent with {p_val} computed from coeffs.")
-        return provided_val
-
-    p_even = _extract_element({ir.p for _, ir in coeffs.irreps if ir.l % 2 == 0})
-    p_odd = _extract_element({ir.p for _, ir in coeffs.irreps if ir.l % 2 == 1})
-    computed_p_val, computed_p_arg = _compute_parities(p_even, p_odd)
-    p_val = _check_consistency(p_val, computed_p_val, "p_val")
-    p_arg = _check_consistency(p_arg, computed_p_arg, "p_arg")
-    return SphericalSignal(signal, quadrature=quadrature, p_val=p_val, p_arg=p_arg)
-
 
 def _rfft(x: jnp.ndarray, l: int) -> jnp.ndarray:
     r"""Real fourier transform
